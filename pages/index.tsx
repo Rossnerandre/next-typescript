65a69ffb0621afb43936162565903e819ae113fc
André Rossner <rossnerandre@gmail.com>
2022-05-26 12:28:14 -0300

    Tests in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,54 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Button, LinearProgress} from '@mui/material';
+import {Formik, Form, Field, FormikHelpers} from 'formik';
+import {TextField as MuiTextField} from '@mui/material';
+import {TextField} from 'formik-mui';
+import * as Yup from 'yup';
+import { IMaskMixin} from "react-imask";
+import {ReactElement} from "react";
+
+const MaskedInput = IMaskMixin(TextField);
+
+interface Values {
+  name: string;
+  lastName: string;
+  email: string;
+}
 
 const Home: NextPage = () => {
+
+  const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    lastName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
+
+  const initialValues: Values = {
+    name: '',
+    lastName: '',
+    email: '',
+  };
+
+  function onSubmit(values:Values, {setSubmitting, setErrors}: FormikHelpers<Values>) {
+    setTimeout(() => {
+      console.log('Form data', values);
+      setSubmitting(false);
+      alert(JSON.stringify(values, null, 2));
+      setErrors({
+        email: 'EMAIL JÁ CADASTRADO',
+      });
+    }, 1000);
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +58,62 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={SignupSchema}
+          onSubmit={onSubmit}>
+          {({submitForm, isSubmitting, touched, errors}) => (
+            <Form>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+              <Field
+                name="name"
+                label="Name"
+                component={MaskedInput}
+                mask={'000.000.000-00'}
+                margin="normal"
+                variant="filled"
+                sx={{
+                  marginBottom: '1em',
+                }}
+              />
+              <br />
+              <Field
+                name="lastName"
+                component={TextField}
+                label="Last Name"
+                margin="normal"
+                variant="filled"
+                sx={{
+                  marginBottom: '1em',
+                }}
+              />
+              <br />
+              <Field
+                component={TextField}
+                name="email"
+                type="email"
+                label="Email"
+                variant="filled"
+                sx={{
+                  marginBottom: '1em',
+                }}
+              />
+              <br />
+              {isSubmitting && <LinearProgress/>}
+              <br/>
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={isSubmitting}
+                onClick={submitForm}
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
